2967a14332c13e1e35fc7d415bb6c0498db472a7
Amit Yadav <amityadavdtu@gmail.com>
2023-10-07 19:32:45 +0530

    Review Star and Checkout Component

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,32 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
-import { Route } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 
 export const App = () => {
   return (
-    <div>
-    <Navbar/>
-    <Route path='/'>
-     <HomePage/>
-    </Route>
-    <SearchBooksPage/>
-    <Footer/>
+    <div className='d-flex flex-column min-vh-100'>
+      <Navbar />
+      <div className='flex-gow-1'>
+        <Switch>
+          {/* Redirect will redirect to the action inside "to" */}
+          <Route path='/' exact>
+            <Redirect to='/home' />
+            <HomePage />
+          </Route>
+          <Route path="/home">
+            <HomePage />
+          </Route>
+          <Route path='/search'>
+            <SearchBooksPage />
+          </Route>
+          <Route path={'/checkout/:bookId'}>
+            <BookCheckoutPage/>
+          </Route>
+        </Switch>
+      </div>
+      <Footer/>
     </div>
   );
 }
